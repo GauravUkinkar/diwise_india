b9604641db6573e914b6ba4c1a91635860801677
candyjo-72 <jyotsna171999@gmail.com>
2024-12-18 10:04:25 +0530

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home/>} />
           <Route path="/about" element={<About/>} />
-          <Route path="/test" element={<Test/>} />
+          {/* <Route path="/test" element={<Test/>} /> */}
         </Routes>
        </BrowserRouter>
 
