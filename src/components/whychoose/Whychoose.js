d47e2ea00633963d06c0b63edd5128e01988b3ca
nitadiwise <nita@diwise.in>
2024-12-18 14:25:41 +0530

    header

--- src/components/whychoose/Whychoose.js
+++ src/components/whychoose/Whychoose.js
@@ -6,29 +6,32 @@ function Whychoose() {
         <>
             <div className=" why-parent parent">
                 <div className="why-container container">
-                    <h3 className="why-choose-heading">Lorem ipsum dolor consectetursit amet,<br /> consectetur adipisicing.</h3>
+                    <h3 className="why-choose-heading">WHY CHOOSE US?</h3>
                     <div className="why-choose-card-box">
                         <div className="why-choose-card">
                             <div className="why-choose-card-text-box">
                                 <div className="why-choose-card-img"></div>
                                 <h4 className=''>TECHNOLOGY</h4>
-                                <p>Technology is a key to leading in any industry. Our technical team </p>
+                                <p>Technology is a key to leading in any industry. Our technical team leverages it to provide streamlined processes and highly dependable solutions for our clients.</p>
                             </div>
 
                         </div>
                         <div className="why-choose-card">
                             <div className="why-choose-card-text-box">
                                 <div className="why-choose-card-img"></div>
-                                <h4 className=''>TECHNOLOGY</h4>
-                                <p>Technology is a key to leading in any industry. Our technical team </p>
+                                <h4 className=''>INTEGRITY</h4>
+                                <p>Honesty and fairness are the guidelines By which we measure all our actions. We dedicate ourselves to walking this moral high ground.
+
+</p>
                             </div>
 
                         </div>
                         <div className="why-choose-card">
                             <div className="why-choose-card-text-box">
                                 <div className="why-choose-card-img"></div>
-                                <h4 className=''>TECHNOLOGY</h4>
-                                <p>Technology is a key to leading in any industry. Our technical team </p>
+                                <h4 className=''>CONTINUOUS IMPROVEMENT
+                                </h4>
+                                <p>Excellence is a continuous process. Keeping up with the latest industrial trends and being up to date with newer technologies is reinforced within the team.</p>
                             </div>
 
                         </div>
