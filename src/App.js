aa9a9617eeee340b5ed75bfb4cc7f55a7748098e
nitadiwise <nita@diwise.in>
2024-12-24 16:42:21 +0530

    contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.scss';
 import About from './pages/about/About';
@@ -7,35 +7,37 @@ import Test from "./pages/test/Test";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 import Scroll from "./components/scroll/Scroll";
 import Blog from "./pages/blog/Blog";
 import Blog_details from "./pages/blogdetails/Blog_details";
+import Contact from "./pages/contact/Contact";
 
 function App() {
 
   useEffect(() => {
-        AOS.init({
-          duration: 1000, // Animation duration (ms)
-          easing: "ease-in-out", // Animation easing
-          once: true, // Whether the animation should happen only once
-        });
-      }, []);
+    AOS.init({
+      duration: 1000, // Animation duration (ms)
+      easing: "ease-in-out", // Animation easing
+      once: true, // Whether the animation should happen only once
+    });
+  }, []);
 
   return (
     <div className="App">
 
       <BrowserRouter>
-      <Scroll/>
-      <Header/>
+        <Scroll />
+        <Header />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/blog" element={<Blog /> } />
-<Route path="/blog_detail" element={<Blog_details /> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog_detail" element={<Blog_details />} />
+          <Route path='contact' element={<Contact/>}  />
           {/* <Route path="/test" element={<Test/>} /> */}
         </Routes>
-      <Footer/>  
+        <Footer />
       </BrowserRouter>
 
     </div>
---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Contact.scss';
+import { PiPhoneLight } from 'react-icons/pi';
+import { MdOutlineMailOutline } from 'react-icons/md';
+import { CiLocationOn } from 'react-icons/ci';
+import { AiOutlineMail, AiTwotoneMail } from 'react-icons/ai';
+
+function Contact() 
+{
+    return (
+
+        <>
+        <div className="contact-parent parent">
+            <div className="contact-container   ">
+                <div
+                    className="contact-left">
+                    <div className="left-cont">
+                        <h2>Contact Us</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <form className='contact-form'>
+                            <input type='text' placeholder='Name' name='name' />
+
+                            <input type='email' placeholder='Email'
+                                name='email'
+                            />
+                            <input type='text' placeholder='Phone' name='phone' />
+                            <textarea rows={3}
+                                placeholder='Message'>
+                            </textarea>
+                            <button className='cta'>Submit</button>
+
+                        </form>
+                    </div>
+                </div>
+                
+                <div className="contact-right">
+                <div className="orange-box"></div>
+                    <div className="right-cont">
+                      
+                        <h3>Info</h3>
+                        <div className="contact-icon-box">
+                            <span><AiOutlineMail /> </span><span>info@gmail.com</span>
+                        </div>
+                        <div className="contact-icon-box">
+                            <span><PiPhoneLight /></span>
+                            <span> +91 9852146940</span>
+                        </div>
+                        <div className="contact-icon-box">
+                            <span><CiLocationOn /></span>
+                            <span>Office No. 214-216, 10 Biz Park,Near Symbiosis Law College, Airport Road,Vimannagar, Pune- 411014</span>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div >
+
+        </>
+    )
+}
+
+export default Contact
