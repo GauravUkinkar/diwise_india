0723441ac7b3f8e428b10466e8ed426ee11521da
candyjo-72 <jyotsna171999@gmail.com>
2024-12-30 10:56:59 +0530

    footer

--- src/components/hook/Hook.js
+++ src/components/hook/Hook.js
@@ -1,30 +1,36 @@
-import {React, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import './Hook.scss';
+import React, { useEffect } from "react";
+import "./Hook.scss";
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 
 function Hook() {
-   useEffect(() => {
-      AOS.init({
-        duration: 1000, // Animation duration (ms)
-        easing: "ease-in-out", // Animation easing
-        once: true, // Whether the animation should happen only once
-      });
-    }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration (ms)
+      easing: "ease-in-out", // Animation easing
+      once: true, // Whether the animation should happen only once
+    });
+  }, []);
+
   return (
-   <>
-   <div className="hook parent ">
-    <div className="hook-container container bg-img-contain" data-aos="zoom-in-up" >
-      
-      <h2 >You Have an <span className='highlight'> Idea?</span></h2>
-      <a href="" className="cta" >Tell Us</a>
-    </div>
-   </div>
-   
-  
-   </>
-  )
+    <>
+      <div className="hook parent">
+        {/* Fading div */}
+        <div className="fade-overlay"></div>
+        <div
+          className="hook-container container bg-img-contain"
+          data-aos="zoom-in-up"
+        >
+          <h2>
+            You Have an <span className="highlight">Idea?</span>
+          </h2>
+          <a href="" className="cta">
+            Tell Us
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Hook
+export default Hook;
