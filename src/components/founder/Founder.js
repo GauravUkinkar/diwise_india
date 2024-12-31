3af298f63601e93457dba0544d84c3a4b97c337b
candyjo-72 <jyotsna171999@gmail.com>
2024-12-31 14:56:00 +0530

    founder

--- src/components/founder/Founder.js
+++ src/components/founder/Founder.js
@@ -5,17 +5,17 @@ import pic from "../../img/prajakta_mam.webp";
 function Founder() {
   return (
     <div className="founder parent">
-      <h2>Meet <span className="highlight">Our Founder</span></h2>
+      <h2 data-aos="fade-up" data-aos-delay="100">Meet <span className="highlight">Our Founder</span></h2>
       <div className="founder-container container">
         <div className="founder-left">
-          <h3>Prajakta Marwaha</h3>
-          <p className="position">(Founder and Managing Director)</p>
-          <p className="description">
+          <h3 data-aos="fade-up" data-aos-delay="200">Prajakta Marwaha</h3>
+          <p className="position" data-aos="fade-up" data-aos-delay="200">(Founder and Managing Director)</p>
+          <p className="description" data-aos="fade-up" data-aos-delay="200">
             As the visionary leader of DIwise Global, Prajakta Marwaha brings unparalleled expertise and a strategic mindset. Her deep understanding of digital marketing and innovative approach drives our team towards exceptional results. Her commitment to understanding client needs and building long-term partnerships ensures sustainable growth and success.
           </p>
-          <a href="#" className="cta">Let's Connect</a>
+          <a href="#" className="cta" data-aos="fade-up" data-aos-delay="200">Let's Connect</a>
         </div>
-        <div className="founder-right bg-img-cover">
+        <div className="founder-right bg-img-cover" data-aos="fade-left" data-aos-delay="200">
           <img src={pic} alt="Prajakta Marwaha" />
         </div>
       </div>
